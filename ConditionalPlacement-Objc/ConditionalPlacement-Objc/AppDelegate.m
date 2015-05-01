d261f56ae570591a9a9068de76a07fe648084004
Jon Thornburg <reliable2192@gmail.com>
2015-04-30 22:35:11 -0600

    conditional placement

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -17,10 +17,38 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
+    NSInteger karl = 7;
+    NSInteger leonard = 7;
     
+    NSInteger karlPlace;
+    NSInteger leonardPlace;
+    
+    karlPlace = karl > 7 ? 1 : 2;
+    leonardPlace = leonard > 7 ? 1 : 2;
+    
+    if ((karlPlace == 1) && (leonardPlace == 1)) {
+        NSLog(@"Both teachers are placed 1");
+    }
+    if (!((karlPlace == 1) && (leonardPlace == 1))) {
+        if (karlPlace == 1) {
+            NSLog(@"Karl is placed 1 and Leonard is placed 2");
+        }
+    }
+    if (!(karlPlace == 1)) {
+        if (leonardPlace == 1 ) {
+            NSLog(@"Leonard is placed 1 and Karl is placed 2");
+        }
+    }
+    if ((!(karlPlace == 1) && (!(leonardPlace == 1)))) {
+        NSLog(@"Both teachers are placed 2.");
+    }
+    
+    
+        
     
     return YES;
-}
 
+
+
+}
 @end
